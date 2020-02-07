bfb7f127218abaaded3e6afc4b785bb4c7a589ed
Law <b.stoltz67@orange.fr>
2020-02-07 16:31:46 +0100

    v1.01

--- src/Entity/Option.php
+++ src/Entity/Option.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\OptionRepository")
- * @Orm\Table(name="`Option`")
+ * @Orm\Table(name="`option`")
  */
 class Option
 {
